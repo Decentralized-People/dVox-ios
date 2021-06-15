b0bd8d37ce42e718210c06bd071cae7dc08c2e55
Aleksandr Molchagin <contact@aleksandrmolchagin.com>
2021-06-15 22:57:07 +0300

    Initial setup

---
+++ ProjectDIES/ProjectDIES/ProjectDIESApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDIESApp.swift
+//  ProjectDIES
+//
+//  Created by Aleksandr Molchagin on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDIESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
