d8360644bae45b58d5b9b0009ea507a93cd8f93f
Revaz Bakuradze <Revaz.Bakuradze18@kzoo.edu>
2021-07-12 16:13:18 +0400

    Adding Random Name Generator

--- dVox/dVox/MainView.swift
+++ dVox/dVox/MainView.swift
@@ -10,17 +10,122 @@ import SwiftUI
 import UIKit
 import MessageUI
 
+var adjectives = [String](arrayLiteral: "Sturdy",
+                    "Loud",
+                    "Delicious",
+                    "Decorous",
+                    "Pricey",
+                    "Knowing",
+                    "Scientific",
+                    "Lazy",
+                    "Fair",
+                    "Loutish",
+                    "Wonderful",
+                    "Strict",
+                    "Gaudy",
+                    "Innocent",
+                    "Horrible",
+                    "Puzzled",
+                    "Homeless",
+                    "Happy",
+                    "Grandiose",
+                    "Observant",
+                    "Pumped",
+                    "Pale",
+                    "Royal",
+                    "Flawless",
+                    "Actual",
+                    "Realistic",
+                    "Cynical",
+                    "Clean",
+                    "Strict",
+                    "Super",
+                    "Powerful",
+                    "Mixed",
+                    "Slim",
+                    "Ubiquitous",
+                    "Faithful",
+                    "Amusing",
+                    "Emotional",
+                    "Staking",
+                    "Former",
+                    "Scarce",
+                    "Tense",
+                    "Black-and-white",
+                    "Tangy",
+                    "Wrong",
+                    "Sloppy",
+                    "Regular",
+                    "Deafening",
+                    "Savory",
+                    "Sturdy",
+                    "Classy",
+                    "Third",
+                    "Valuable",
+                    "Outgoing",
+                    "Free",
+                    "Terrific",
+                    "Sleepy",
+                    "Adorable",
+                    "Cozy")
+
+var animals = [String](arrayLiteral: "Boar",
+                   "Koala",
+                   "Snake",
+                   "Frog",
+                   "Parrot",
+                   "Lion",
+                   "Pig",
+                   "Rhino",
+                   "Sloth",
+                   "Horse",
+                   "Sheep",
+                   "Chameleon",
+                   "Giraffe",
+                   "Yak",
+                   "Cat",
+                   "Dog",
+                   "Penguin",
+                   "Elephant",
+                   "Fox",
+                   "Otter",
+                   "Gorilla",
+                   "Rabbit",
+                   "Raccoon",
+                   "Wolf",
+                   "Panda",
+                   "Goat",
+                   "Chicken",
+                   "Duck",
+                   "Cow",
+                   "Ray",
+                   "Catfish",
+                   "Ladybug",
+                   "Dragonfly",
+                   "Owl",
+                   "Beaver",
+                   "Alpaca",
+                   "Mouse",
+                   "Walrus",
+                   "Kangaroo",
+                   "Butterfly",
+                   "Jellyfish",
+                   "Deer",
+                   "Beetle",
+                   "Tiger",
+                   "Pigeon",
+                   "Bearded dragon",
+                   "Bat",
+                   "Hippo",
+                   "Crocodile",
+                   "Monkey")
+
 struct MainView: View {
     
     var body: some View {
         
-        
-    Text("hello world")
+    Text("Welcome " + randomNameGenerator())
             
-        
-        
-           
-        
     }
     
 
@@ -31,4 +136,14 @@ struct MainView_Previews: PreviewProvider {
                     
             }
         }
+    
+    func randomNameGenerator() -> String {
+        
+        let randomAdjective = Int.random(in: 0..<adjectives.count)
+        let randomAnimal = Int.random(in: 0..<animals.count)
+        
+        let randomName = adjectives[randomAdjective] + " " + animals[randomAnimal]
+        
+        return randomName
+    }
 }
